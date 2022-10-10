4aea50c379ab91998321b43a682f35418f6344cc
sumit <97390822+Sumit9953@users.noreply.github.com>
2022-10-10 23:47:04 +0530

    .

--- ARRAY/array_intro.cpp
+++ ARRAY/array_intro.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int arr[5] = {2,4,5};
+    int arr[5] = {2,4,5,4};
     int n = sizeof(arr)/sizeof(arr[0]);
     cout<<*(arr+1)<<" ";
     for (int i = 0; i < n; i++)
