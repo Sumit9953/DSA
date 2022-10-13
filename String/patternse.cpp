fbef97cccecf8dcc7909d47a61a60b89a1df1f11
sumit <97390822+Sumit9953@users.noreply.github.com>
2022-10-13 14:03:21 -0400

    string

---
+++ String/patternse.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int pattern(string &s1,string &s2){
+    int m = s1.length();
+    int n = s2.length();
+    for (int i = 0; i <= m-n; i++)
+    {
+        int j;
+        for (j = 0; j < n; j++)
+        {
+            if(s1[i+j] != s2[j]){
+                break;
+            }
+        }
+        if(j == n){
+            return i;
+        }
+    }
+    
+}
+
+int main(){
+    string s1 = "abgcdabcd";
+    string s2 = "abcd";
+    cout<<endl;
+    cout<<pattern(s1,s2)<<endl;
+}
